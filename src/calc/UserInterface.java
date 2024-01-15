6d1c13cee3decf63f70c1ccb4eb97d786b480a7e
SculptorLux <alexanisin@gmail.com>
2024-01-15 07:33:57 +0300

    ОНО РАБОТАЕТ

--- src/calc/UserInterface.java
+++ src/calc/UserInterface.java
@@ -8,6 +8,7 @@ public class UserInterface {
         System.out.println("Введите выражение");
         Scanner scanner = new Scanner(System.in);
         String inputScanner = scanner.nextLine();
+        RomanConverter romanConverter = new RomanConverter();
         String result = Main.calc(inputScanner);
         System.out.println(result);
         scanner.close();
